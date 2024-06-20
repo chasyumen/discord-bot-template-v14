7a75a3e0d57b5f54d579dea96721c56c9e364453
chasyumen <54601672+chasyumen@users.noreply.github.com>
2024-06-20 23:42:36 +0900

    Fix component locale bug

---
+++ src/locale/en_US/components.js
@@ -0,0 +1,10 @@
+export const id = "components";
+export const language = "en_US";
+
+export const data = {
+    "components": {
+        "help": {
+            "otherUser": "This help panel was created by other user. Please run /help command by your self.",
+        }
+    }
+};
---
+++ src/locale/ja/components.js
@@ -0,0 +1,10 @@
+export const id = "components";
+export const language = "ja";
+
+export const data = {
+    "components": {
+        "help": {
+            "otherUser": "このヘルプ一覧は他のユーザーによって作成されました。 /helpコマンドで自分用にヘルプ一覧を表示できます。",
+        }
+    }
+};
