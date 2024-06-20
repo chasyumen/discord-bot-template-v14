a8c9452763a9072e252c98f2928a361fec48add8
chasyumen <54601672+chasyumen@users.noreply.github.com>
2024-06-20 09:08:24 +0900

    glob update

--- src/utils/getDir.js
+++ src/utils/getDir.js
@@ -1,16 +1,18 @@
-import glob from "glob";
+import { glob } from "glob";
 
 export default async function (dir) {
-    var getDirectories = function (src, callback) {
-        glob(src + '/**/*', callback);
-    };
-    return await new Promise(async (resolve, reject) => {
-        getDirectories(dir, function (err, res) {
-            if (err) {
-                reject(err);
-            } else {
-                resolve(res);
-            }
-        });
-    });
+    // var getDirectories = function (src, callback) {
+    //     glob(src + '/**/*', callback);
+    // };
+    // return await new Promise(async (resolve, reject) => {
+    //     getDirectories(dir, function (err, res) {
+    //         if (err) {
+    //             reject(err);
+    //         } else {
+    //             resolve(res);
+    //         }
+    //     });
+    // });
+    var dirs = await glob(dir + '/**/*');
+    return dirs.map(dirname => {return dirname.replace(/\\/g, "/")});
 }
